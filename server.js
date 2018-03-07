f84938beb6ff4cfb40bc217e9c667dbb7318490b
Meena <mukeshmeena86@gmail.com>
2018-03-07 17:54:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello This is A Node JS APP Finally Deployed - We are testing CICD pipeline for non.com\n');
+  res.send('Hello This is A Node JS APP Finally Deployed - We are testing CICD pipeline for noon.com\n');
 });
 
 app.listen(PORT, HOST);
